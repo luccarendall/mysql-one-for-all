560893e4d4fd99be6b5471c32980506aa36acf07
Lucca Rendall <luccarendall2@gmail.com>
2022-07-28 01:19:06 -0300

    Fix: Os comentários com (-------) que usei para separar as tabelas estavam quebrando os testes

--- desafio1.sql
+++ desafio1.sql
@@ -1,23 +1,31 @@
-DROP  DATABASE  IF  EXISTS SpotifyClone;
+DROP DATABASE IF EXISTS SpotifyClone;
 
-CREATE  DATABASE SpotifyClone;
+CREATE DATABASE IF NOT EXISTS SpotifyClone;
 
--- Modelo:
--- CREATE TABLE SpotifyClone.tabela1(
--- coluna1 tipo restricoes,
--- coluna2 tipo restricoes,
--- colunaN tipo restricoes,
--- ) engine = InnoDB; => Engine padrão
+CREATE TABLE IF NOT EXISTS SpotifyClone.plano (
+  plano_id INT NOT NULL AUTO_INCREMENT,
+  plano VARCHAR(45) NOT NULL,
+  valor DECIMAL(5,2) NOT NULL,
+  PRIMARY KEY (plano_id))
+ENGINE = InnoDB;
+
+INSERT INTO SpotifyClone.plano (plano, valor)
+VALUES 
+  ("gratuito", 0),
+  ("familiar", 7.99),
+  ("universitário", 5.99),
+  ("pessoal", 6.99);
+
 
 CREATE TABLE IF NOT EXISTS SpotifyClone.usuario (
-  usuario_id INT NOT NULL AUTO_INCREMENT,
-  usuario VARCHAR(45) NOT NULL,
-  idade INT NOT NULL,
-  plano_id INT NOT NULL,
-  data_assinatura DATE NOT NULL,
-  PRIMARY KEY (usuario_id),
-    FOREIGN KEY (plano_id)
-    REFERENCES SpotifyClone.plano(plano_id)
+    usuario_id INT NOT NULL AUTO_INCREMENT,
+    usuario VARCHAR(45) NOT NULL,
+    idade INT NOT NULL,
+    plano_id INT NOT NULL,
+    data_assinatura DATE NOT NULL,
+    PRIMARY KEY (usuario_id),
+      FOREIGN KEY (plano_id)
+      REFERENCES SpotifyClone.plano(plano_id)
 )
 ENGINE = InnoDB;
 
@@ -34,30 +42,15 @@ VALUES
   ("Angelina", 42, 2, "2018-04-29"),
   ("Paul", 46, 2, "2017-01-17");
 
----------------------------------------------
-CREATE TABLE IF NOT EXISTS SpotifyClone.plano (
-  plano_id INT NOT NULL AUTO_INCREMENT,
-  plano VARCHAR(45) NOT NULL,
-  valor DECIMAL(5,2) NOT NULL,
-  PRIMARY KEY (plano_id))
-ENGINE = InnoDB;
 
-INSERT INTO SpotifyClone.plano (plano, valor)
-VALUES 
-  ("gratuito", 0),
-  ("familiar", 7.99),
-  ("universitário", 5.99),
-  ("pessoal", 6.99);
-
-  ---------------------------------------------
   CREATE TABLE IF NOT EXISTS SpotifyClone.artista (
   artista_id INT NOT NULL AUTO_INCREMENT,
-  nome VARCHAR(45) NOT NULL,
+  artista VARCHAR(45) NOT NULL,
   PRIMARY KEY (artista_id))
-ENGINE = InnoDB;
+  ENGINE = InnoDB;
 
-INSERT INTO SpotifyClone.artista (artista)
-VALUES
+  INSERT INTO SpotifyClone.artista (artista)
+  VALUES
   ("Walter Phoenix"),
   ("Peter Strong"),
   ("Lance Day"),
@@ -65,8 +58,8 @@ VALUES
   ("Tyler Isle"),
   ("Fog");
 
----------------------------------------------
-CREATE TABLE IF NOT EXISTS SpotifyClone.album (
+
+  CREATE TABLE IF NOT EXISTS SpotifyClone.album (
   album_id INT NOT NULL AUTO_INCREMENT,
   album_name VARCHAR(45) NOT NULL,
   artista_id INT NOT NULL,
@@ -74,12 +67,12 @@ CREATE TABLE IF NOT EXISTS SpotifyClone.album (
   PRIMARY KEY (album_id),
   FOREIGN KEY (artista_id)
   REFERENCES SpotifyClone.artista(artista_id)
-)
-ENGINE = InnoDB;
+  )
+  ENGINE = InnoDB;
 
 
-INSERT INTO SpotifyClone.album (album_name, artista_id, ano_lancamento)
-VALUES 
+  INSERT INTO SpotifyClone.album (album_name, artista_id, ano_lancamento)
+  VALUES 
   ("Envious", 1, 1990),
   ("Exuberant", 1, 1993),
   ("Hallowed Steam", 2, 1995),
@@ -91,8 +84,8 @@ VALUES
   ("No guarantees", 5, 2015),
   ("Apparatus", 6, 2015);
 
----------------------------------------------
-CREATE TABLE IF NOT EXISTS SpotifyClone.musica (
+
+  CREATE TABLE IF NOT EXISTS SpotifyClone.musica (
   musica_id INT NOT NULL AUTO_INCREMENT,
   musica VARCHAR(45) NOT NULL,
   duracao_segundos INT NOT NULL,
@@ -100,11 +93,11 @@ CREATE TABLE IF NOT EXISTS SpotifyClone.musica (
   PRIMARY KEY (musica_id),
     FOREIGN KEY (album_id)
     REFERENCES SpotifyClone.album(album_id)
-)
-ENGINE = InnoDB;
+  )
+  ENGINE = InnoDB;
 
-INSERT INTO SpotifyClone.musica (musica, duracao_segundos, album_id)
-VALUES 
+  INSERT INTO SpotifyClone.musica (musica, duracao_segundos, album_id)
+  VALUES 
   ("Soul For Us", 200, 1),
   ("Reflections Of Magic", 163, 1),
   ("Dance With Her Own", 116, 1),
@@ -155,8 +148,8 @@ VALUES
   ("Baby", 136, 10),
   ("You Make Me Feel So..", 83, 10);
 
----------------------------------------------
-CREATE TABLE IF NOT EXISTS SpotifyClone.seguindo (
+
+  CREATE TABLE IF NOT EXISTS SpotifyClone.seguindo (
   usuario_id INT NOT NULL,
   artista_id INT NOT NULL,
   PRIMARY KEY (usuario_id, artista_id),
@@ -166,11 +159,11 @@ CREATE TABLE IF NOT EXISTS SpotifyClone.seguindo (
 
     FOREIGN KEY (usuario_id)
     REFERENCES SpotifyClone.usuario(usuario_id)
-)
-ENGINE = InnoDB;
+  )
+  ENGINE = InnoDB;
 
-INSERT INTO SpotifyClone.seguindo (usuario_id, artista_id)
-VALUES 
+  INSERT INTO SpotifyClone.seguindo (usuario_id, artista_id)
+  VALUES 
   (1, 1),
   (1, 4),
   (1, 3),
@@ -194,8 +187,8 @@ VALUES
   (10, 2),
   (10, 6);
 
----------------------------------------------
-CREATE TABLE IF NOT EXISTS SpotifyClone.historico_reproducao (
+
+  CREATE TABLE IF NOT EXISTS SpotifyClone.historico_reproducao (
   usuario_id INT NOT NULL,
   musica_id INT NOT NULL,
   data_hora_reproducao DATETIME NOT NULL,
@@ -204,46 +197,46 @@ CREATE TABLE IF NOT EXISTS SpotifyClone.historico_reproducao (
   REFERENCES SpotifyClone.usuario(usuario_id),
   FOREIGN KEY (musica_id)
   REFERENCES SpotifyClone.musica(musica_id)
-)
-ENGINE = InnoDB;
+  )
+  ENGINE = InnoDB;
 
-INSERT INTO SpotifyClone.historico_reproducao(usuario_id, musica_id, data_hora_reproducao)
-VALUES
-(1, 36, '2020-02-28 10:45:55'),
-(1, 25, '2020-05-02 05:30:35'),
-(1, 23, '2020-03-06 11:22:33'),
-(1, 14, '2020-08-05 08:05:17'),
-(1, 15, '2020-09-14 16:32:22'),
-(2, 34, '2020-01-02 07:40:33'),
-(2, 24, '2020-05-16 06:16:22'),
-(2, 21, '2020-10-09 12:27:48'),
-(2, 39, '2020-09-21 13:14:46'),
-(3, 6, '2020-11-13 16:55:13'),
-(3, 3, '2020-12-05 18:38:30'),
-(3, 26, '2020-07-30 10:00:00'),
-(4, 2, '2021-08-15 17:10:10'),
-(4, 35, '2021-07-10 15:20:30'),
-(4, 27, '2021-01-09 01:44:33'),
-(5, 7, '2020-07-03 19:33:28'),
-(5, 12, '2017-02-24 21:14:22'),
-(5, 14, '2020-08-06 15:23:43'),
-(5, 1, '2020-11-10 13:52:27'),
-(6, 38, '2019-02-07 20:33:48'),
-(6, 29, '2017-01-24 00:31:17'),
-(6, 30, '2017-10-12 12:35:20'),
-(6, 22, '2018-05-29 14:56:41'),
-(7, 5, '2018-05-09 22:30:49'),
-(7, 4, '2020-07-27 12:52:58'),
-(7, 11, '2018-01-16 18:40:43'),
-(8, 39, '2018-03-21 16:56:40'),
-(8, 40, '2020-10-18 13:38:05'),
-(8, 32, '2019-05-25 08:14:03'),
-(8, 33, '2021-08-15 21:37:09'),
-(9, 16, '2021-05-24 17:23:45'),
-(9, 17, '2018-12-07 22:48:52'),
-(9, 8, '2021-03-14 06:14:26'),
-(9, 9, '2020-04-01 03:36:00'),
-(10, 20, '2017-02-06 08:21:34'),
-(10, 21, '2017-12-04 05:33:43'),
-(10, 12, '2017-07-27 05:24:49'),
-(10, 13, '2017-12-25 01:03:57'); 
+  INSERT INTO SpotifyClone.historico_reproducao(usuario_id, musica_id, data_hora_reproducao)
+  VALUES
+  (1, 36, '2020-02-28 10:45:55'),
+  (1, 25, '2020-05-02 05:30:35'),
+  (1, 23, '2020-03-06 11:22:33'),
+  (1, 14, '2020-08-05 08:05:17'),
+  (1, 15, '2020-09-14 16:32:22'),
+  (2, 34, '2020-01-02 07:40:33'),
+  (2, 24, '2020-05-16 06:16:22'),
+  (2, 21, '2020-10-09 12:27:48'),
+  (2, 39, '2020-09-21 13:14:46'),
+  (3, 6, '2020-11-13 16:55:13'),
+  (3, 3, '2020-12-05 18:38:30'),
+  (3, 26, '2020-07-30 10:00:00'),
+  (4, 2, '2021-08-15 17:10:10'),
+  (4, 35, '2021-07-10 15:20:30'),
+  (4, 27, '2021-01-09 01:44:33'),
+  (5, 7, '2020-07-03 19:33:28'),
+  (5, 12, '2017-02-24 21:14:22'),
+  (5, 14, '2020-08-06 15:23:43'),
+  (5, 1, '2020-11-10 13:52:27'),
+  (6, 38, '2019-02-07 20:33:48'),
+  (6, 29, '2017-01-24 00:31:17'),
+  (6, 30, '2017-10-12 12:35:20'),
+  (6, 22, '2018-05-29 14:56:41'),
+  (7, 5, '2018-05-09 22:30:49'),
+  (7, 4, '2020-07-27 12:52:58'),
+  (7, 11, '2018-01-16 18:40:43'),
+  (8, 39, '2018-03-21 16:56:40'),
+  (8, 40, '2020-10-18 13:38:05'),
+  (8, 32, '2019-05-25 08:14:03'),
+  (8, 33, '2021-08-15 21:37:09'),
+  (9, 16, '2021-05-24 17:23:45'),
+  (9, 17, '2018-12-07 22:48:52'),
+  (9, 8, '2021-03-14 06:14:26'),
+  (9, 9, '2020-04-01 03:36:00'),
+  (10, 20, '2017-02-06 08:21:34'),
+  (10, 21, '2017-12-04 05:33:43'),
+  (10, 12, '2017-07-27 05:24:49'),
+  (10, 13, '2017-12-25 01:03:57'); 
