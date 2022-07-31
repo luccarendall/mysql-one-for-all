e91c37705120a401bfb4c7ac60104d65d3e30690
Lucca Rendall <luccarendall2@gmail.com>
2022-07-31 00:38:39 -0300

    Desafio3

---
+++ desafio3.sql
@@ -0,0 +1,17 @@
+SELECT 
+  DISTINCT user.usuario  AS usuario,
+
+  COUNT(hR.usuario_id) AS qtde_musicas_ouvidas,
+
+  ROUND(SUM(DISTINCT mus.duracao_segundos)/60, 2)  AS total_minutos
+
+FROM SpotifyClone.usuario AS user
+
+INNER JOIN SpotifyClone.historico_reproducao AS hR
+  ON user.usuario_id = hR.usuario_id
+
+INNER JOIN SpotifyClone.musica AS c
+  ON hR.musica_id = mus.musica_id
+
+GROUP BY
+  usuario
