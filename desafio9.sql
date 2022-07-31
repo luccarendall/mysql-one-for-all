c51d56ec1394ac63298313111a2ee17c577fbbe9
Lucca Rendall <luccarendall2@gmail.com>
2022-07-31 00:39:15 -0300

    Desafio9

--- desafio9.sql
+++ desafio9.sql
@@ -1,1 +1,8 @@
-q
+SELECT COUNT(hR.cancao_id) AS quantidade_musicas_no_historico
+
+FROM SpotifyClone.historico_reproducao AS hR
+
+INNER JOIN SpotifyClone.usuario AS user
+  ON hR.usuario_id = user.usuario_id
+
+WHERE user.usuario = "Bill"
