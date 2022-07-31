8e2b41cd5e95c1ff1f43721491bb8dedd8cbf223
Lucca Rendall <luccarendall2@gmail.com>
2022-07-31 00:38:57 -0300

    Desafio4

---
+++ desafio4.sql
@@ -0,0 +1,12 @@
+SELECT 
+  
+  DISTINCT user.usuario AS usuario,
+  IF( MAX(YEAR(hR.data_reproducao)) = 2021, "Usuário ativo", "Usuário inativo") AS condicao_usuario
+
+FROM SpotifyClone.usuario AS user
+
+INNER JOIN SpotifyClone.historico_reproducao AS hR
+  ON user.usuario_id = hR.usuario_id
+
+GROUP BY
+  usuario
