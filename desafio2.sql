ba8b9fe947ad368356d297e807b301809a8a5cd8
Lucca Rendall <luccarendall2@gmail.com>
2022-07-31 00:36:17 -0300

    Revert "Refactor: adicionando alias"
    This reverts commit d3718929839be6357789b7b9b35bc86a739727a0.

--- desafio2.sql
+++ desafio2.sql
@@ -1,8 +1,8 @@
 SELECT 
-  COUNT(DISTINCT music.musica_id)  AS cancoes, 
-  COUNT(DISTINCT artist.artista_id) AS artistas, 
-  COUNT(DISTINCT alb.album_id) AS albuns
-FROM
-  SpotifyClone.musica AS music, 
-  SpotifyClone.artista AS artist, 
-  SpotifyClone.album AS alb; 
+  COUNT(DISTINCT SpotifyClone.musica.musica_id)  AS cancoes, 
+  COUNT(DISTINCT SpotifyClone.artista.artista_id) AS artistas, 
+  COUNT(DISTINCT SpotifyClone.album.album_id) AS albuns
+FROM 
+  SpotifyClone.musica, 
+  SpotifyClone.artista, 
+  SpotifyClone.album; 
